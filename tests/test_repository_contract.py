2f5bca12844deb1b469849e690afc7f214fe0509
Desire <Desire>
2026-08-23 18:25:28 +0200

    chore: stop tracking tests temporarily

--- tests/test_repository_contract.py
+++
@@ -1,9 +0,0 @@
-from rag_sec.database.repositories import IngestionRepository, ProcessingRepository
-
-
-def test_ingestion_repository_has_get_by_id():
-    assert callable(IngestionRepository.get_by_id)
-
-
-def test_processing_repository_has_get_by_id():
-    assert callable(ProcessingRepository.get_by_id)
